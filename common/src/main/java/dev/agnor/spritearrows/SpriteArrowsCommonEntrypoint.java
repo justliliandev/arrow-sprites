17b19b10aee89f52b85c433b4234a413925a96cd
agnor99 <kreader100@gmail.com>
2023-05-21 19:35:10 +0200

    update version number

--- common/src/main/java/dev/agnor/spritearrows/SpriteArrowsCommonEntrypoint.java
+++ common/src/main/java/dev/agnor/spritearrows/SpriteArrowsCommonEntrypoint.java
@@ -1,23 +1,19 @@
 package dev.agnor.spritearrows;
 
 
-import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.client.renderer.entity.ArrowRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.EntityRenderers;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SpriteArrowsCommonEntrypoint {
     public static final String MOD_ID = "spritearrows";
-    public static final Logger LOGGER = LogUtils.getLogger();
 
     public static void replace() {
         Minecraft instance = Minecraft.getInstance();
@@ -28,11 +24,10 @@ public class SpriteArrowsCommonEntrypoint {
                 if (entry.getKey() == EntityType.TRIDENT || entry.getValue().create(ctx) instanceof ArrowRenderer<?>) {
                     replaceable.add(entry.getKey());
                 }
-            } catch (Exception ignored) {
+            } catch (Exception | NoClassDefFoundError ignored) {
             }
         }
         for (EntityType type: replaceable) {
-            SpriteArrowsCommonEntrypoint.LOGGER.warn("at replace: " + BuiltInRegistries.ENTITY_TYPE.getKey(type));
             EntityRenderers.PROVIDERS.put(type, provide());
         }
     }
