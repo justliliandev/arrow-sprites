ef54c6cf4a1acbde48d39412fc742ac4e53103e0
agnor99 <36055315+agnor99@users.noreply.github.com>
2023-06-28 17:30:22 +0200

    delay renderer replacement on fabric a bit. might increase compat with other mods idk

--- fabric/src/main/java/dev/agnor/spritearrows/fabric/SpriteArrowsFabricEntrypoint.java
+++ fabric/src/main/java/dev/agnor/spritearrows/fabric/SpriteArrowsFabricEntrypoint.java
@@ -2,11 +2,13 @@ package dev.agnor.spritearrows.fabric;
 
 import dev.agnor.spritearrows.SpriteArrowsCommonEntrypoint;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 
 public class SpriteArrowsFabricEntrypoint implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        SpriteArrowsCommonEntrypoint.replace();
+        ClientLifecycleEvents.CLIENT_STARTED.register(mc -> SpriteArrowsCommonEntrypoint.replace());
+        //SpriteArrowsCommonEntrypoint.replace();
     }
 }
